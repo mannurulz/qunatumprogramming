ab6c52883332cfffd1bf7e45d6603e702fa60c46
mannurulz <mannurulz@gmail.com>
2018-10-25 18:34:45 +0800

    Hello World - Quantum

---
+++ Q_Hello_World.py
@@ -0,0 +1,9 @@
+import cirq
+
+# define the length of the grid.
+length = 3
+# define qubits on the grid.
+qubits = [cirq.GridQubit(i, j) for i in range(length) for j in range(length)]
+print(qubits)
+# prints 
+# [GridQubit(0, 0), GridQubit(0, 1), GridQubit(0, 2), GridQubit(1, 0), GridQubit(1, 1), GridQubit(1, 2), GridQubit(2, 0), GridQubit(2, 1), GridQubit(2, 2)]
