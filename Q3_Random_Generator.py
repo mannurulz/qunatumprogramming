417f4e757c0add9e49160d85b609d8ba4363150a
mannurulz <mannurulz@gmail.com>
2018-10-29 15:35:42 +0800

    Random Generator

---
+++ Q3_Random_Generator.py
@@ -0,0 +1,17 @@
+import random
+def rand2d(rows, cols):
+    return [[random.choice([+1, -1]) for _ in range(rows)] for _ in range(cols)]
+
+def random_instance(length):
+    # transverse field terms
+    h = rand2d(length, length)
+    # links within a row
+    jr = rand2d(length, length - 1)
+    # links within a column
+    jc = rand2d(length - 1, length)
+    return (h, jr, jc)
+    
+h, jr, jc = random_instance(3)
+print('transverse fields: {}'.format(h))
+print('row j fields: {}'.format(jr))
+print('column j fields: {}'.format(jc))
