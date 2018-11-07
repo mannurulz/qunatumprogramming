eda9dae33e76d95ef2dc9ae33a78769e2ebf1f2c
mannurulz <mannurulz@gmail.com>
2018-11-07 16:24:52 +0800

    v2

--- Q6_Schedule_v1.py
+++ Q6_Schedule_v1.py
@@ -1,3 +1,39 @@
+#Defining Device
+import cirq
+from cirq.devices import GridQubit
+from cirq.google import XmonGate
+class Xmon10Device(cirq.Device):
+
+  def __init__(self):
+      self.qubits = [GridQubit(i, 0) for i in range(10)]
+
+  def duration_of(self, operation):
+      # Wouldn't it be nice if everything took 10ns?
+      return cirq.Duration(nanos=10)
+
+  def validate_operation(self, operation):
+      if (not isinstance(operation, cirq.GateOperation) or
+              not isinstance(operation.gate, XmonGate)):
+          raise ValueError('{!r} is not an XmonGate'.format(operation))
+      if len(operation.qubits) == 2:
+          p, q = operation.qubits
+          if not p.is_adjacent(q):
+            raise ValueError('Non-local interaction: {}'.format(repr(operation)))
+
+
+  def validate_scheduled_operation(self, schedule, scheduled_operation):
+      self.validate_operation(scheduled_operation.operation)
+
+  def validate_circuit(self, circuit):
+      for moment in circuit:
+          for operation in moment.operations:
+              self.validate_operation(operation)
+
+  def validate_schedule(self, schedule):
+      for scheduled_operation in schedule.scheduled_operations:
+          self.validate_scheduled_operation(schedule, scheduled_operation)
+
+# Defining and using schedule over ExpWGate
 from cirq.google.xmon_gates import ExpWGate
 circuit = cirq.Circuit()
 CZ = Exp11Gate(half_turns=1.0)
